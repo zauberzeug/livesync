e73da82362bba423826e99ff3a016e55e2e9be9d
Rodja Trappe <rodja@zauberzeug.com>
2022-09-11 10:49:51 +0200

    add test for ignoring mtime

---
+++ tests/test_ignoring_mtime.sh
@@ -0,0 +1,13 @@
+. ~/assert.sh
+set -e
+
+cd /root
+mkdir -p /target/my_project
+touch /target/my_project/file.txt
+mkdir -p my_project
+cd my_project
+sleep 2
+touch file.txt
+livesync target &
+sleep 2
+assert_gt $(stat -c %Y file.txt) $(stat -c %Y /target/my_project/file.txt) "mtime should be different"
