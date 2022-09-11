b2dfdfeeffd2304b10d67c3db77203fcaa41dd3c
Rodja Trappe <rodja@zauberzeug.com>
2022-09-11 07:47:20 +0200

    add test to sync multiple folders defined in vscode workspace

---
+++ tests/test_syncing_vscode_workspace_with_multiple_folders.sh
@@ -0,0 +1,19 @@
+. ~/assert.sh
+set -e
+
+cd /root
+mkdir -p my_project
+mkdir -p another_project
+echo 'file created' > another_project/data.txt
+cd my_project
+echo 'file created' > file.txt
+echo '{
+  "folders": [ { "path": "." }, { "path": "../another_project" }]}' > my_project.code-workspace
+livesync target &
+sleep 1
+assert_eq "file created" "$(cat /target/my_project/file.txt)" "wrong file content"
+echo 'file changed' > ../another_project/data.txt
+echo 'file changed' > file.txt
+sleep 1
+assert_eq "file changed" "$(cat /target/my_project/file.txt)" "wrong file content"
+assert_eq "file changed" "$(cat /target/another_project/data.txt)" "wrong data content"
