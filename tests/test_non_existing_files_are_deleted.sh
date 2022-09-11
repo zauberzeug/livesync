8ac3ebd8e2c775377ab8d46d8d0e72282e7473a6
Rodja Trappe <rodja@zauberzeug.com>
2022-09-11 07:39:18 +0200

    add test to ensure non-exisiting source files are removed from target

---
+++ tests/test_non_existing_files_are_deleted.sh
@@ -0,0 +1,11 @@
+. ~/assert.sh
+set -e
+
+cd /root
+mkdir -p my_project
+cd my_project
+mkdir -p /target/my_project/
+touch /target/my_project/file.txt
+livesync --source . target &
+sleep 1
+assert_eq 0 "$(find /target -name '*txt' | wc -l)" "file on target should have been deleted, because its not present in source"
