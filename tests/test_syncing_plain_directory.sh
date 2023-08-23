511b74dcc8f5278d042aa2b99edd3c2d4ba7ebb5
Dennis Wittich <dennis@zauberzeug.com>
2023-08-23 09:44:22 +0200

    increase sleep times

--- tests/test_syncing_plain_directory.sh
+++ tests/test_syncing_plain_directory.sh
@@ -6,10 +6,10 @@ mkdir -p my_project
 cd my_project
 echo 'file created' > file.txt
 livesync --mutex-interval 1 target &
-sleep 2
-"$(cat /target/my_project/file.txt)"
+sleep 5
+echo "$(cat /target/my_project/file.txt)"
 assert_eq "file created" "$(cat /target/my_project/file.txt)" "wrong file content"
 echo 'file changed' > file.txt
-sleep 2
-"$(cat /target/my_project/file.txt)"
+sleep 5
+echo "$(cat /target/my_project/file.txt)"
 assert_eq "file changed" "$(cat /target/my_project/file.txt)" "wrong file content"
