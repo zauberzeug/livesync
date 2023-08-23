a0ddc40a5538dfd4db9e122329d9a007aed6287b
Dennis Wittich <dennis@zauberzeug.com>
2023-08-23 09:36:40 +0200

    print file content to see why tests fail

--- tests/test_on_change_command.sh
+++ tests/test_on_change_command.sh
@@ -6,11 +6,12 @@ mkdir -p my_project
 cd my_project
 echo 'file created' > file.txt
 livesync --on-change "mktemp ../onchange-XXXXXXXX" target &
-sleep 2
+sleep 5
+echo "$(cat /target/my_project/file.txt)"
 assert_eq "file created" "$(cat /target/my_project/file.txt)" "wrong file content"
 echo 'new file content' > file.txt
 assert_eq 1 "$(find /target -name 'onchange-*' | wc -l)" "on-change should have been called once on init"
-sleep 2
+sleep 5
 echo "$(cat /target/my_project/file.txt)"
 assert_eq "new file content" "$(cat /target/my_project/file.txt)" "wrong file content"
 assert_eq 2 "$(find /target -name 'onchange-*' | wc -l)" "on-change should have been called two times"
