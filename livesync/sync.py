5faa247d91049e4c79c3a62fa8065fc15aa7b61e
Dominik Drees <dominik@zauberzeug.com>
2026-02-04 11:40:45 +0100

    Stop watch tasks if renewing mutex fails
    Fixes #23.

--- livesync/sync.py
+++ livesync/sync.py
@@ -28,16 +28,32 @@ async def run_folder_tasks(
             await folder.sync()
 
         if watch:
+            tasks = []
             for folder in folders:
                 print(f'Watch folder {folder.source_path}', flush=True)
-                asyncio.create_task(folder.watch())
+                tasks.append((folder, asyncio.create_task(folder.watch())))
 
             while True:
                 if not ignore_mutex:
                     summary = get_summary(folders)
-                    for mutex in mutexes.values():
-                        if not await mutex.set(summary):
-                            break
+
+                    hosts_to_check = list(mutexes.keys())
+                    for host in hosts_to_check:
+                        if not await mutexes[host].set(summary):
+                            print(
+                                f'Target {host} is in use by {mutexes[host].occupant}, stopping watch tasks', flush=True)
+                            new_tasks = []
+                            for f, t in tasks:
+                                if f.host == host:
+                                    t.cancel()
+                                else:
+                                    new_tasks.append((f, t))
+                            tasks = new_tasks
+                            del mutexes[host]
+
+                    if not tasks:
+                        print('No more folders to watch, exiting', flush=True)
+                        break
                 await asyncio.sleep(mutex_interval)
     except Exception as e:
         print(e)
